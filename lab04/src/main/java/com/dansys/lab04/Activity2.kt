538d63e593c8e97d45f2e3433186c08c1b5960da
Danila Sysolatin <danilasysolatin.ed@gmail.com>
2023-05-01 21:44:30 +0600

    В Activity 2 разместите date picker/time picker и кнопку ОК. При нажатии на ОК сделайте set result и передайте дату. Это можно сделать, например с помощью getMillis (возвращает long – кол-во мс с 1970 г).
    3. В Activity 1 примите и выведите дату.

--- lab04/src/main/java/com/dansys/lab04/Activity2.kt
+++ lab04/src/main/java/com/dansys/lab04/Activity2.kt
@@ -1,17 +1,62 @@
 package com.dansys.lab04
 
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class Activity2 : AppCompatActivity() {
+    private var dateInMillis: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        val name = intent.getStringExtra("name")
-        nameTextView.text = "Hello, $name!"
+        val datePickerButton = findViewById<Button>(R.id.datepicker_button)
+        datePickerButton.setOnClickListener {
+            showDatePicker()
+        }
+
+        val okButton = findViewById<Button>(R.id.ok_button)
+        okButton.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("date", dateInMillis)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                calendar.set(year, month, dayOfMonth)
+                showTimePicker(calendar.timeInMillis)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+
+    private fun showTimePicker(dateInMillis: Long) {
+        val calendar = Calendar.getInstance()
+        TimePickerDialog(
+            this,
+            { _, hourOfDay, minute ->
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                calendar.set(Calendar.MINUTE, minute)
+                this.dateInMillis = calendar.timeInMillis
+            },
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        ).show()
     }
 }
--- lab04/src/main/java/com/dansys/lab04/MainActivity.kt
+++ lab04/src/main/java/com/dansys/lab04/MainActivity.kt
@@ -5,21 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import java.text.DateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val submitBtn = findViewById<Button>(R.id.submitBtn)
+        nameEditText = findViewById(R.id.name_edittext)
 
-        submitBtn.setOnClickListener {
-            val name = nameEditText.text.toString()
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
             val intent = Intent(this, Activity2::class.java)
-            intent.putExtra("name", name)
-            startActivity(intent)
+            intent.putExtra("name", nameEditText.text.toString())
+            startActivityForResult(intent, 1)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val dateInMillis = data?.getLongExtra("date", 0)
+            val dateString = getTimeString(dateInMillis ?: 0)
+            val textView = findViewById<TextView>(R.id.result_textview)
+            textView.text = "Hello ${nameEditText.text}! Your chosen date is $dateString."
+        }
+    }
+
+    private fun getTimeString(millis: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = millis
+        return DateFormat.getDateInstance().format(calendar.time)
     }
 }
